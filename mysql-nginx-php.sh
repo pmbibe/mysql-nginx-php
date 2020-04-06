c083cb2b4cd120eef68b99821082c9f3235f78a6
pmbibe <53257014+pmbibe@users.noreply.github.com>
2020-04-06 15:54:48 +0700

    Update mysql-nginx-php.sh

--- mysql-nginx-php.sh
+++ mysql-nginx-php.sh
@@ -28,7 +28,3 @@ yum -y update && \
 yum -y install php && \
 yum -y install php-fpm php-gd php-json php-mbstring php-mysqlnd php-xml php-xmlrpc php-opcache
 systemctl start php-fpm
-#mysql
-yum -y localinstall https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
-yum -y install mysql-community-server
-grep 'A temporary password' /var/log/mysqld.log |tail -1
